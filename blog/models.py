fd45a76045cacd24920ea0bfcbb18dc655bfbde7
Beatriz Nieves <nievesb0429@gmail.com>
2023-04-19 21:31:35 -0500

    adding new like model and likes relationship on post amd comments

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 from django.utils import timezone
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 
@@ -13,6 +15,7 @@ class Post(models.Model):
     img = models.ImageField(upload_to='uploads/images/%Y/%m/%d/', blank=True, null=True)
     safe = models.BooleanField(default=True)
     tagged_users = models.ManyToManyField(User, through='UserTag', related_name='tagged_posts')
+    likes = GenericRelation('Like')
 
     @property
     def comments_count(self):
@@ -42,5 +45,17 @@ class Comment(models.Model):
     blogpost = models.ForeignKey(Post, on_delete=models.CASCADE, null=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
+    likes = GenericRelation('Like')
     def __str__(self):
         return self.body
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.content_type.model} {self.object_id}"
